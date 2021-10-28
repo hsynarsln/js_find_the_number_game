650e806ac602fb5d54585164b23cf8cc12f8897a
hsynarsln <90923579+hsynarsln@users.noreply.github.com>
2021-10-29 00:28:37 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,26 +5,34 @@ let count = 0;
 let output = document.getElementsByClassName('attempts')[0];
 let guesses = document.getElementsByClassName('guesses')[0];
 let submitBtn = document.getElementById('submit');
+let input = document.getElementById('input');
 
 function checkGuess() {
-  let guessNum = document.querySelector('#guess').value;
+  let guessNum = input.value;
   if (guessNum == randomNum) {
     h2.innerHTML = `Congratulations, you have guessed, it's ${guessNum}`;
     count++;
     guesses.innerText += ` ${guessNum}`;
     output.innerText = `Total attempts : ${count}`;
+    input.value = '';
   }
   if (guessNum > randomNum) {
     h2.innerText = 'Down!';
     count++;
     guesses.innerText += ` ${guessNum}`;
     output.innerText = `Number of attempts: ${count}`;
+    input.value = '';
   }
   if (guessNum < randomNum) {
     h2.innerText = 'Up!';
     count++;
     guesses.innerText += ` ${guessNum}`;
     output.innerText = `Number of attempts: ${count}`;
+    input.value = '';
+  }
+  if (guessNum > 100 || guessNum <= 0) {
+    alert('Invalid number.. Please enter a number between 1 and 100');
+    input.value = '';
   }
 }
 
